ef3396d8f7aeb467ee452922e0aa41c433592ab9
Bartosz Bieniek <itsbk20@gmail.com>
2023-01-14 17:55:18 +0100

    Move MainActivity into presentation package

--- app/src/main/java/pl/bk20/forest/presentation/MainActivity.kt
+++ app/src/main/java/pl/bk20/forest/presentation/MainActivity.kt
@@ -1,8 +1,9 @@
-package pl.bk20.forest
+package pl.bk20.forest.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import pl.bk20.forest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
