8e3f66c51dac38df9ce0388dde9b948cbc0515a4
Bartosz Bieniek <itsbk20@gmail.com>
2023-01-14 17:26:48 +0100

    Setup MainActivity's layout

--- app/src/main/java/pl/bk20/forest/MainActivity.kt
+++ app/src/main/java/pl/bk20/forest/MainActivity.kt
@@ -1,10 +1,12 @@
 package pl.bk20.forest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
