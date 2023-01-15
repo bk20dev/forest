73138f7738d0957c9777f7232b4ffddf0d4d5e83
Bartosz Bieniek <itsbk20@gmail.com>
2023-01-15 16:23:40 +0100

    Implement SensorEventListener for StepCounterService

--- app/src/main/java/pl/bk20/forest/service/StepCounterService.kt
+++ app/src/main/java/pl/bk20/forest/service/StepCounterService.kt
@@ -1,17 +1,26 @@
 package pl.bk20.forest.service
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Build
 import android.os.Build.VERSION_CODES
 import android.os.IBinder
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import pl.bk20.forest.R
 
-class StepCounterService : Service() {
+class StepCounterService : Service(), SensorEventListener {
+
+    private var sensorManager: SensorManager? = null
 
     companion object {
         private const val NOTIFICATION_CHANNEL_ID = "step_counter_channel"
@@ -23,15 +32,35 @@ class StepCounterService : Service() {
             val notificationChannel = createNotificationChannel()
             registerNotificationChannel(notificationChannel)
         }
+        sensorManager = (getSystemService(Context.SENSOR_SERVICE) as SensorManager)
+            .also { registerStepCounter(it) }
+        val notification = createNotification()
+        startForeground(NOTIFICATION_ID, notification)
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+    private fun createNotification(): Notification =
+        NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
             .setOngoing(true)
+            .build()
 
-        startForeground(NOTIFICATION_ID, notification.build())
+    private fun registerStepCounter(sensorManager: SensorManager) {
+        // TODO: Check for android.permission.ACTIVITY_RECOGNITION permission
+        val stepCounterSensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        stepCounterSensor?.let {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
 
-        return START_STICKY
+    override fun onSensorChanged(event: SensorEvent?) {
+        Log.d("StepCounterService", "Sensor changed ${event?.values?.toList()}")
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.d("StepCounterService", "Accuracy changed $accuracy")
+    }
+
+    override fun onDestroy() {
+        sensorManager?.unregisterListener(this)
     }
 
     override fun onBind(intent: Intent?): IBinder? = null
