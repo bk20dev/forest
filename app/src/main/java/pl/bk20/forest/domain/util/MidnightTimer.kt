8c9fba536aa8efd1051c0f2b557b274f759206d4
Bartosz Bieniek <itsbk20@gmail.com>
2023-01-20 14:46:37 +0100

    Create MidnightTimer

---
+++ app/src/main/java/pl/bk20/forest/domain/util/MidnightTimer.kt
@@ -0,0 +1,24 @@
+package pl.bk20.forest.domain.util
+
+import java.time.Duration
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+class MidnightTimer(
+    private val timer: Timer,
+    private val task: TimerTask
+) {
+    fun start() {
+        val delay = millisToMidnight(Instant.now())
+        timer.schedule(task, delay, Duration.ofDays(1))
+    }
+
+    fun stop() {
+        timer.cancel()
+    }
+
+    private fun millisToMidnight(at: Instant): Long {
+        val midnight = at.truncatedTo(ChronoUnit.DAYS) + Duration.ofDays(1)
+        return midnight.toEpochMilli() - at.toEpochMilli()
+    }
+}
---
+++ app/src/main/java/pl/bk20/forest/domain/util/TimerTask.kt
@@ -0,0 +1,5 @@
+package pl.bk20.forest.domain.util
+
+fun interface TimerTask {
+    fun run()
+}
