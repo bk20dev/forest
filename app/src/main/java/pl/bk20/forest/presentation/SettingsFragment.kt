8c9808aa57debdf9ebc75f728b4df01d8891cfe6
Bartosz Bieniek <itsbk20@gmail.com>
2023-01-25 19:31:23 +0100

    Set EditTextPreference input types Create daily goal summary provider

--- app/src/main/java/pl/bk20/forest/presentation/SettingsFragment.kt
+++ app/src/main/java/pl/bk20/forest/presentation/SettingsFragment.kt
@@ -1,7 +1,10 @@
 package pl.bk20.forest.presentation
 
 import android.os.Bundle
+import android.text.InputType
 import android.view.Menu
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import pl.bk20.forest.R
 
@@ -9,8 +12,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        @Suppress("DEPRECATION")
-        setHasOptionsMenu(true)
+        @Suppress("DEPRECATION") setHasOptionsMenu(true)
     }
 
     @Suppress("OVERRIDE_DEPRECATION")
@@ -20,6 +22,19 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
-    }
 
+        val dailyGoalPreference = preferenceManager.findPreference<EditTextPreference>("daily_goal")
+        dailyGoalPreference?.summaryProvider = Preference.SummaryProvider<EditTextPreference> {
+            val dailyGoal = it.text?.toIntOrNull() ?: 0
+            resources.getQuantityString(R.plurals.daily_goal_summary, dailyGoal, dailyGoal)
+        }
+
+        val numericPreferenceKeys = listOf("daily_goal", "step_length", "height", "weight")
+        numericPreferenceKeys.forEach {
+            val preference = preferenceManager.findPreference<EditTextPreference>(it)
+            preference?.setOnBindEditTextListener { editText ->
+                editText.inputType = InputType.TYPE_CLASS_NUMBER
+            }
+        }
+    }
 }
