fa49e007d308eec12db75d5fca3ca2e4c9ebefb3
Bartosz Bieniek <itsbk20@gmail.com>
2023-01-31 21:17:02 +0100

    Create stats fragment

--- app/src/main/java/pl/bk20/forest/presentation/MainActivity.kt
+++ app/src/main/java/pl/bk20/forest/presentation/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import pl.bk20.forest.R
@@ -32,7 +33,14 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        setupActionBarWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.progressFragment,
+                R.id.statsFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
         binding.bottomNavigation.setupWithNavController(navController)
 
         // TODO: Ask for permissions or something
@@ -42,10 +50,6 @@ class MainActivity : AppCompatActivity() {
     private fun startStepCounterService() {
         val intent = Intent(this, StepCounterService::class.java)
         ContextCompat.startForegroundService(this, intent)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
---
+++ app/src/main/java/pl/bk20/forest/presentation/StatsFragment.kt
@@ -0,0 +1,5 @@
+package pl.bk20.forest.presentation
+
+import androidx.fragment.app.Fragment
+
+class StatsFragment : Fragment()
