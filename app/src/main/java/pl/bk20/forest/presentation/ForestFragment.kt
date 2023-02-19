fd35a6e4d28be763adb3547573cd5ed71451598e
Bartosz Bieniek <itsbk20@gmail.com>
2023-02-19 23:55:35 +0100

    Create ForestViewModel

--- app/src/main/java/pl/bk20/forest/presentation/ForestFragment.kt
+++ app/src/main/java/pl/bk20/forest/presentation/ForestFragment.kt
@@ -5,9 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
 import pl.bk20.forest.databinding.FragmentForestBinding
 
 class ForestFragment : Fragment() {
+
+    private val viewModel: ForestViewModel by viewModels { ForestViewModel.Factory }
 
     private lateinit var binding: FragmentForestBinding
 
@@ -18,5 +25,17 @@ class ForestFragment : Fragment() {
     ): View {
         binding = FragmentForestBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED) {
+                viewModel.trees.collect { updateUserInterface(it) }
+            }
+        }
+    }
+
+    private fun updateUserInterface(forestState: ForestState) {
+        binding.textTreesCollected.text = forestState.treeCount.toString()
     }
 }
---
+++ app/src/main/java/pl/bk20/forest/presentation/ForestViewModel.kt
@@ -0,0 +1,44 @@
+package pl.bk20.forest.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import pl.bk20.forest.ForestApplication
+import pl.bk20.forest.data.repository.DayRepositoryImpl
+import pl.bk20.forest.domain.usecase.ForestUseCases
+
+class ForestViewModel(
+    forestUseCases: ForestUseCases
+) : ViewModel() {
+
+    private val _trees = MutableStateFlow(ForestState(treeCount = 0))
+    val trees: StateFlow<ForestState> = _trees.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            forestUseCases.getTreeCount().collect {
+                _trees.value = _trees.value.copy(
+                    treeCount = it
+                )
+            }
+        }
+    }
+
+    object Factory : ViewModelProvider.Factory {
+
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+            val application = checkNotNull(extras[APPLICATION_KEY]) as ForestApplication
+            val forestDatabase = application.forestDatabase
+            val dayRepository = DayRepositoryImpl(forestDatabase.dayDao)
+            val forestUseCases = ForestUseCases(dayRepository)
+            return ForestViewModel(forestUseCases) as T
+        }
+    }
+}
