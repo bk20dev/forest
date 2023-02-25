8e08fd80493a5b3b085a9831c363f7308f703acb
Bartosz Bieniek <itsbk20@gmail.com>
2023-02-25 11:11:55 +0100

    Add view pager to stats fragment

--- app/src/main/java/pl/bk20/forest/presentation/StatsFragment.kt
+++ app/src/main/java/pl/bk20/forest/presentation/StatsFragment.kt
@@ -5,7 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import pl.bk20.forest.databinding.FragmentStatsBinding
+
+class StatsPageAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment = when (position) {
+        0 -> DailyStatsFragment()
+        else -> StatsSummaryFragment()
+    }
+}
 
 class StatsFragment : Fragment() {
 
@@ -18,5 +29,13 @@ class StatsFragment : Fragment() {
     ): View {
         binding = FragmentStatsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val statsPageAdapter = StatsPageAdapter(this)
+        binding.pager.apply {
+//            isUserInputEnabled = false
+            adapter = statsPageAdapter
+        }
     }
 }
